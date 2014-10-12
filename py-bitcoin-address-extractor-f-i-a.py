c3cde829cb53ae40de7df43e24448d2af903784f
bitcoins.sg <bitcoinssg@gmail.com>
2014-10-12 16:36:54 +0545

    .

--- py-bitcoin-address-extractor-f-i-a.py
+++ py-bitcoin-address-extractor-f-i-a.py
@@ -26,6 +26,7 @@ start_time=datetime.datetime.now()
 def main():
 	parser = argparse.ArgumentParser(description='python script to parse bitcoin addresses from insight api\'s level db')
 	parser.add_argument('-testnet', action='store_true', dest='db_type', help='turn this on to process testnet db instead of livenet', required=False)
+	parser.add_argument('-sort', action='store_true', dest='sorted', help='turn this on to sort addresses', required=False)
 	parser.add_argument('-d', action='store', dest='directory_for_db', help='if db directory is not default (i.e. ~/.insight..); specify', required=False)
 	parser.add_argument('-o', action='store', dest='output_file', help='\033[31m' +'(Rq)' + '\033[0m' + ' output file (e.g. extracted_add.txt)', required=True)
 	#parser.add_argument('-anon', action='store_true', dest='route_via_tor', help='route via tor or not', required=False)
@@ -45,6 +46,90 @@ def main():
         extraction_core_txs_optimized_two(directory=args.directory_for_db,args=args)
 
 
+
+
+def extraction_core_txs_optimized_two(directory,args):
+	output_file = codecs.open(args.output_file, 'w', 'utf8')
+	logging.info("processing db dir: " + directory + "/txs")
+	logging.info("algorithm: extraction core optimized 2")
+	if(args.sorted):
+		logging.info("sorting: True")
+	else:
+		logging.info("sorting: False")
+	number_of_addreses=0
+	count=0
+	show_interval = 1000
+	currentaddress=["-thisisatemplateusedforbootstrapping-",0]
+	currentaddress[1]=len(currentaddress[0])
+	list_of_addresses=""
+	for key in plyvel.DB(directory+"/txs", create_if_missing=False):
+		#print key[0]
+		if key[0][0:4] != "txa2":
+			break
+		if not currentaddress[0] == key[0][4:currentaddress[1]+4]:
+			currentaddress[0] = key[0][4:6+(key[0][5:].index("-"))]
+			currentaddress[1]=len(currentaddress[0])
+			list_of_addresses=list_of_addresses+currentaddress[0][1:]
+			number_of_addreses+=1
+			#output_file.write(currentaddress[0] + '\n')
+		count+=1
+		if (count % show_interval) == 0:
+			logging.info('processed ' + str(count) +' txs')
+			show_interval = show_interval * 2
+	logging.info('processed ' + str(count) +' txs .. '+  'done')
+	output_file.write(list_of_addresses)
+	output_file.close()
+	#for item in list_of_addresses:
+		#output_file.write("%s\n" % item)
+	if(args.sorted):
+		logging.info('sorting addresses')
+		subprocess.call(["tr '-' '\n' < " + args.output_file +" | sort -u -o " + args.output_file],shell=True)
+	else:
+		logging.info('processing file')
+		subprocess.call(["tr '-' '\n' < " + args.output_file +" > " + "temp_" + args.output_file],shell=True)
+		subprocess.call(["mv -f" + " temp_" + args.output_file + "  " + args.output_file],shell=True)
+	#subprocess.call(["awk -F'-' '{print $2}' " + args.output_file + " | sort -u -o " + args.output_file], shell=True)
+	logging.info('output file: ' + args.output_file)
+	logging.info("total time: " + '\033[92m' + (datetime.datetime.now()-start_time).__str__().split('.')[0] + '\033[0m' + ' on ' + datetime.datetime.today().strftime('%b, %d, %Y') )
+	logging.info('# of addresses: ' + str(number_of_addreses))
+	logging.info('\033[92m' + 'completed.'+ '\033[0m')
+	print("") 
+	exit(0)
+
+
+def print_logo(indent="           "):
+	# print ello logo
+	new_lines_before=1
+	new_lines_after=2
+	for i in range(1,new_lines_before):
+		print("")
+	print("         py-bitcoin-address-extractor-f-i-a")
+	print("-------------------------------------------------------")
+	print("      python script to parse bitcoin addresses        ")
+	print("            from insight api's level db ")
+	print("                    version: Alpha")
+	print("-------------------------------------------------------")
+	print("")
+	print(" o                                                    ")
+	print("O     o                  o                            ")
+	print("O        O                                            ")
+	print("o       oOo                                           ")
+	print("OoOo. O  o   .oOo  .oOo. O 'OoOo. .oOo     .oOo  .oOoO")
+	print("O   o o  O   O     O   o o  o   O `Ooo.    `Ooo. o   O")
+	print("o   O O  o   o     o   O O  O   o     O        O O   o")
+	print("`OoO' o' `oO `OoO' `OoO' o  o   O `OoO'  O `OoO' `OoOo")
+	print("                                                     O ")
+	print("16QcZYETFbWRijK3xBVbDgpvW1ZWsdNujY                OoO' ")    
+	for i in range(1,new_lines_after):
+		print("")
+	# Initialize default logging behaviour
+
+def print_after_end(indent="           "):
+	print("\033[92m" + "@bitcoinsg" + "\033[0m")
+
+
+
+# old functions
 def extraction_core_txs(directory,args):
 	output_file = codecs.open(args.output_file, 'w', 'utf8')
 	logging.info("processing db dir: " + directory + "/txs")
@@ -94,73 +179,5 @@ def extraction_core_txs_optimized(directory,args):
 	logging.info('\033[92m' + 'completed.'+ '\033[0m')
 	print("") 
 	exit(0)
-
-def extraction_core_txs_optimized_two(directory,args):
-	output_file = codecs.open(args.output_file, 'w', 'utf8')
-	logging.info("processing db dir: " + directory + "/txs")
-	logging.info("algorithm: extraction core optimized 2")
-	count=0
-	show_interval = 1000
-	currentaddress=["-thisisatemplateusedforbootstrapping-",0]
-	currentaddress[1]=len(currentaddress[0])
-	list_of_addresses=""
-	for key in plyvel.DB(directory+"/txs", create_if_missing=False):
-		#print key[0]
-		if key[0][0:4] != "txa2":
-			break
-		if not currentaddress[0] == key[0][4:currentaddress[1]+4]:
-			currentaddress[0] = key[0][4:6+(key[0][5:].index("-"))]
-			currentaddress[1]=len(currentaddress[0])
-			list_of_addresses=list_of_addresses+currentaddress[0][1:]
-			#output_file.write(currentaddress[0] + '\n')
-		count+=1
-		if (count % show_interval) == 0:
-			logging.info('processed ' + str(count) +' txs')
-			show_interval = show_interval * 2
-	logging.info('processed ' + str(count) +' txs .. '+  'done')
-	output_file.write(list_of_addresses)
-	#for item in list_of_addresses:
-		#output_file.write("%s\n" % item)
-	logging.info('sorting and removing redundancies')
-	output_file.close()
-	subprocess.call(["tr '-' '\n' < " + args.output_file +" | sort -u -o " + args.output_file],shell=True)
-	#subprocess.call(["awk -F'-' '{print $2}' " + args.output_file + " | sort -u -o " + args.output_file], shell=True)
-	logging.info('output file: ' + args.output_file)
-	logging.info("total time: " + '\033[92m' + (datetime.datetime.now()-start_time).__str__().split('.')[0] + '\033[0m' + ' on ' + datetime.datetime.today().strftime('%b, %d, %Y') )
-	logging.info('\033[92m' + 'completed.'+ '\033[0m')
-	print("") 
-	exit(0)
-
-
-def print_logo(indent="           "):
-	# print ello logo
-	new_lines_before=1
-	new_lines_after=2
-	for i in range(1,new_lines_before):
-		print("")
-	print("         py-bitcoin-address-extractor-f-i-a")
-	print("-------------------------------------------------------")
-	print("      python script to parse bitcoin addresses        ")
-	print("            from insight api's level db ")
-	print("                    version: Alpha")
-	print("-------------------------------------------------------")
-	print("")
-	print(" o                                                    ")
-	print("O     o                  o                            ")
-	print("O        O                                            ")
-	print("o       oOo                                           ")
-	print("OoOo. O  o   .oOo  .oOo. O 'OoOo. .oOo     .oOo  .oOoO")
-	print("O   o o  O   O     O   o o  o   O `Ooo.    `Ooo. o   O")
-	print("o   O O  o   o     o   O O  O   o     O        O O   o")
-	print("`OoO' o' `oO `OoO' `OoO' o  o   O `OoO'  O `OoO' `OoOo")
-	print("                                                     O ")
-	print("16QcZYETFbWRijK3xBVbDgpvW1ZWsdNujY                OoO' ")    
-	for i in range(1,new_lines_after):
-		print("")
-	# Initialize default logging behaviour
-
-def print_after_end(indent="           "):
-	print("\033[92m" + "@bitcoinsg" + "\033[0m")
-
 if __name__ == '__main__':
     main()
